05a47a037ffb9e17ad4b834964fba2a7f07ca714
LuisLugoz <32311316+LuisLugoz@users.noreply.github.com>
2024-05-05 21:27:12 -0500

    Add files via upload

--- ejercicios_practica/js/ejercicio_4.js
+++ ejercicios_practica/js/ejercicio_4.js
@@ -11,19 +11,19 @@
 // 4. Realice un acumulador utilizando una variable, debe acumular en 5 lineas
 //    de código el numero 100, comenzando desde el 1.
 
-/* ELIMINE ESTE COMENTARIO
+
 let acumulador = 1; //No utilizar mas variables ;D
 //primer paso:
-
+acumulador += 9;
 //segundo paso:
-
+acumulador = 10 + acumulador;
 //tercer paso:
-
+acumulador = 20 + acumulador;
 //cuarto paso:
-
+acumulador = 10 + acumulador;
 //quinto paso:
-
+acumulador = 50 + acumulador;
 //Imprimimos en consola:
 console.log("El resultado del programa es: " + acumulador); 
 // El resultado debe ser 100
-*/
+
